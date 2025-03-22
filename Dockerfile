422f72db3f29008cbf00e020c5f128f13054acf5
Vineesh Chauhan <chauhanvineesh@gmail.com>
2025-03-22 22:48:25 +0530

    Docker Full Stack Example

--- Dockerfile
+++ Dockerfile
@@ -22,3 +22,14 @@ CMD node app.js
 #Pull the image from Docker Hub
 #docker pull <YOUR_DOCKER_HUB_USERNAME>/<REPOSITORY_NAME>:<TAG>
 #docker pull chauhanvineesh/codewithmost:latest
+
+
+#docker pull postgres:17.4
+#docker run -d --name postgres-db --network my-network -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=database -p 5432:5432 postgres:17.4
+
+#docker run -d --name backend-server --network my-network -p 8080:8080 backend-server
+
+#server : localhost
+#database : postgres
+#username : postgres
+#password : database
---
+++ SpringBoot/src/main/java/com/example/demo/Users.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
